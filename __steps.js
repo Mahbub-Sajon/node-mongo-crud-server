777cd81f6017ec37e6aa0192f43c7d0a1cee4684
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-04-30 12:55:56 +0600

    deleted added

--- __steps.js
+++ __steps.js
@@ -36,6 +36,11 @@
  * 3. collection.find(query)
  * 4. cursor.toArray()
  * 5. return the result
+ * 6. from client useEffect and display data in UI 
+ * -------------------------------------------------
+ * DELETE
+ * -------------------------------------------------
+ * 
  * 
  * 
  */
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -40,6 +41,15 @@ app.post('/user', async(req, res) =>{
   const result = await userCollection.insertOne(newUser);
   res.send(result);
 });
+
+//delete a user
+app.delete('/user/:id', async(req, res) => {
+  const id = req.params.id;
+  const query = {_id: ObjectId(id)};
+  const result = await userCollection.deleteOne(query);
+  res.send(result);
+})
+
 
 }
   finally {
